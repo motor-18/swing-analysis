aaf3654ecf24cca218f2ea428ba50d0dfb3c31ce
SLS <ssaldanh@andrew.cmu.edu>
2019-11-19 08:47:00 +0400

    Updated comments of functions for code style.

--- analysis.py
+++ analysis.py
@@ -10,17 +10,27 @@ class errorDefinitions():
 errd = errorDefinitions()
 
 """
+Sliding window method:
+Common to the 4 functions below.
+Two loops used.
+
+First loop:
+Initialises the window at indexStart.
+For that window, gets a count for suitable values
+
+Second loop:
+After that, move window by 1, there will be a departing and incoming element
+check departing and incoming element if they are suitable.
+If former, decrement by 1; if latter, increment by 1.
+Check every loop in case the count matches the block size, and store/return the index
+"""
+
+"""
 searchContinuityAboveThreshold:
 Function to search if all elements of a certain length are above the threshold
 Checks using a "sliding window method".
-Initialises the window at indexStart.
-For that window, gets a count for the suitable (suitable = above threshold)
-After that, move window by 1, there will be a departing and incoming element
-check departing and incoming element if above threshold.
-If former, decrement by 1; if latter, increment by 1.
-Check every loop in case the count matches the winLength, return the index of start
-Else return None
-None also returned in case of missing data, small data etc.
+Returns an error value if there is one.
+Else returns the first index which matches criterion.
 """
 def searchContinuityAboveThreshold(data, indexBegin, indexEnd, threshold, winLength):
     count_above_thres = 0
@@ -61,7 +71,9 @@ def searchContinuityAboveThreshold(data, indexBegin, indexEnd, threshold, winLen
 """
 backSearchContinuityWithinRange:
 Function to "back-search" a window of data within a threshold
-Uses same sliding window technique as in searchContinuityAboveValue
+Uses sliding window technique.
+Returns an error value if found.
+Else returns the index value as per criterion.
 """
 def backSearchContinuityWithinRange(data, indexBegin, indexEnd, thresholdLo, thresholdHi, winLength):
     count_within_range = 0
@@ -154,4 +166,47 @@ def searchContinuityAboveValueTwoSignals(
             return i-1
         
     return errd.ERROR_NOT_FOUND
+    #end of function
+
+"""
+searchMultiContinuityWithinRange
+Function that returns all indices at which there is a continuous block of data within a range
+Follows the sliding window idea of previous functions as well
+Returns a list of all indices that match criterion
+"""
+def searchMultiContinuityWithinRange(data, indexBegin, indexEnd, thresholdLo, thresholdHi, winLength):
+    count_within_range = 0
+    suitable_indices = []
+
+    # preliminary error checking
+    if (indexEnd - indexBegin + 1) < winLength:
+        return errd.ERROR_TOO_SMALL
+    if (data is None):
+        return errd.ERROR_NO_DATA
+    
+    #initial work - starting the sliding window
+    for i in range(indexBegin, indexBegin + winLength, 1):
+        try:
+            data[i] = data[i]
+        except IndexError:
+            return errd.ERROR_OUT_OF_BOUNDS
+        if ((data[i] > thresholdLo) and (data[i] < thresholdHi)):
+            count_within_range += 1
+    if (count_within_range == winLength):
+        suitable_indices.append(indexBegin)
+    
+    # moving the sliding window
+    for i in range(indexBegin + 1, indexEnd + 1, 1):
+        try:
+            data[i + winLength - 1] = data[i + winLength - 1]
+        except IndexError:
+            return errd.ERROR_OUT_OF_BOUNDS
+        if ((data[i - 1] > thresholdLo) and (data[i - 1] < thresholdHi)):
+            count_within_range -= 1
+        if ((data[i + winLength - 1] > thresholdLo) and (data[i + winLength - 1] < thresholdHi)):
+            count_within_range += 1
+        if (count_within_range == winLength):
+            suitable_indices.append(i-1)
+    
+    return suitable_indices
     #end of function
